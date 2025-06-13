44959a80df932d58981f792776695dd771cb4a11
Daniel Tran <danieltran354@gmail.com>
2025-06-13 11:18:34 +0200

    version with tilemap

--- src/js/game.js
+++ src/js/game.js
@@ -43,11 +43,8 @@ export class Game extends Engine {
         const player = new Player();
         this.add(player);
 
-<<<<<<< HEAD
-=======
 
 
->>>>>>> 34f5616e8b3a9a272338cb8f8d98025467144efb
         // const waterEnemy = new Enemy();
         // this.add(waterEnemy)
     }
--- src/js/resources.js
+++ src/js/resources.js
@@ -2,20 +2,6 @@ import { ImageSource, Sound, Resource, Loader, SpriteSheet } from 'excalibur'
 
 // voeg hier jouw eigen resources toe
 const Resources = {
-<<<<<<< HEAD
-    // Fish: new ImageSource('images/fish.png'),
-    // WalkingNorthWest: new ImageSource('images/walk_NW_prototype_sprite.png'),
-    // waterEnemyIdle: new ImageSource('images/water_enemy/corrupt_water_idle.png'),
-    idleSouth: new ImageSource('/images/idle_full_sprite_South.png'),
-    walkingNorth: new ImageSource('/images/walk_complete_sprite_North.png'),
-    walkingNorthEast: new ImageSource('/images/walk_complete_sprite_North-East.png'),
-    walkingEast: new ImageSource('/images/walk_complete_sprite_East.png'),
-    walkingSouthEast: new ImageSource('/images/walk_complete_sprite_South-East.png'),
-    walkingSouth: new ImageSource('/images/walk_complete_sprite_South.png'),
-    walkingSouthWest: new ImageSource('/images/walk_complete_sprite_South-West.png'),
-    walkingWest: new ImageSource('/images/walk_complete_sprite_West.png'),
-    walkingNorthWest: new ImageSource('/images/walk_complete_sprite_North-West.png'),
-=======
     idleSouth: new ImageSource('images/idle_full_sprite_South.png'),
 
     walkingNorth: new ImageSource('images/walk_complete_sprite_North.png'),
@@ -26,7 +12,6 @@ const Resources = {
     walkingSouthWest: new ImageSource('images/walk_complete_sprite_South-West.png'),
     walkingWest: new ImageSource('images/walk_complete_sprite_West.png'),
     walkingNorthWest: new ImageSource('images/walk_complete_sprite_North-West.png'),
->>>>>>> 34f5616e8b3a9a272338cb8f8d98025467144efb
 }
 
 const playerIdleSouth = SpriteSheet.fromImageSource({
@@ -39,14 +24,6 @@ const playerIdleSouth = SpriteSheet.fromImageSource({
     }
 })
 
-<<<<<<< HEAD
-// const waterEnemyIdle = SpriteSheet.fromImageSource({
-//     image: Resources.waterEnemyIdle,
-//     grid: { rows: 1, columns: 4, spriteHeight: 96, spriteWidth: 96 }
-// })
-
-=======
->>>>>>> 34f5616e8b3a9a272338cb8f8d98025467144efb
 const playerWalkingNorth = SpriteSheet.fromImageSource({
     image: Resources.walkingNorth,
     grid: {
