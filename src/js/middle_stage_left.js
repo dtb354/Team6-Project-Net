3e771500076663491ef8d77adcd843e0a227b656
Daniel Tran <danieltran354@gmail.com>
2025-06-20 00:16:01 +0200

    middle level has been integrated

--- src/js/middle_stage_left.js
+++ src/js/middle_stage_left.js
@@ -1,4 +1,5 @@
-import { Scene } from "excalibur";
+import { Scene, Vector, Axis, Actor, Color, CollisionType } from "excalibur";
+import { Player } from "./player";
 
 export class Next extends Scene {
     game; 
@@ -10,5 +11,22 @@ export class Next extends Scene {
             console.log("gamepad detected")
             engine.mygamepad = connectevent.gamepad
         })
+
+        // Create player only once
+        const player = new Player();
+        player.pos = new Vector(13, 440);
+        this.add(player);
+
+        // Add tilemap
+        engine.middleLevelMap.addToScene(this);
+
+
+
+
+
+        // Setup camera
+        this.camera.strategy.lockToActorAxis(player, Axis.X);
+        this.camera.strategy.lockToActorAxis(player, Axis.Y);
+        this.camera.zoom = 1.5;
     }
 }
