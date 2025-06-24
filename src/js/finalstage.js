e7cfa5ef68d00bdc4735c72d8e3d87c947633e6b
Daniel Tran <danieltran354@gmail.com>
2025-06-24 13:30:31 +0200

    basic enemies added to final stage

--- src/js/finalstage.js
+++ src/js/finalstage.js
@@ -35,6 +35,42 @@ export class FinalStage extends Scene {
         player.pos = new Vector(-11, -124);
         this.add(player);
 
+        const enemy1 = new Enemy();
+        enemy1.pos = new Vector(-24, -355);
+        this.add(enemy1);
+
+        const enemy2 = new windEnemy();
+        enemy2.pos = new Vector(387, -430);
+        this.add(enemy2);
+
+        const enemy3 = new Enemy();
+        enemy3.pos = new Vector(247, -800);
+        this.add(enemy3);
+
+        const enemy4 = new Enemy();
+        enemy4.pos = new Vector(800, -795);
+        this.add(enemy4);
+
+        const enemy5 = new windEnemy();
+        enemy5.pos = new Vector(545, -1100);
+        this.add(enemy5);
+
+        const enemy6 = new Enemy();
+        enemy6.pos = new Vector(-379, -1100);
+        this.add(enemy6);
+
+        const enemy7 = new windEnemy();
+        enemy7.pos = new Vector(-45, -1280);
+        this.add(enemy7);
+
+        const enemy8 = new windEnemy();
+        enemy8.pos = new Vector(-685, -564);
+        this.add(enemy8);
+
+        const enemy9 = new Enemy();
+        enemy9.pos = new Vector(-327, -795);
+        this.add(enemy9);
+
 
         // Setup camera
         this.camera.strategy.lockToActorAxis(player, Axis.X);
